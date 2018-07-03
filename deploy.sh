558e39dbb9c00547124d5804546f95df1d7cd2bd
jewu <jeremy.wu@blackboard.com>
2018-07-03 15:58:04 +0800

    debugging travis

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,14 @@ if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
       echo "Travis should not deploy from pull requests" # After test integrated 
       exit 0  # here would trigger npm run test instead
     else
+      echo ${GH_REPO}
+      echo ${USER_EMAIL}
+      echo ${USER_NAME}
       git config user.name "${USER_NAME}"
       git config user.email "${USER_EMAIL}"
       git add .
       git commit -m "Update github pages [skip ci]"
       git push "https://${GITHUB_TOKEN}@${GH_REPO}" master
+    fi
+  fi
+fi
