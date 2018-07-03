cfe093157f15ba1cdada90957baf6dca9ddb2d13
jewu <jeremy.wu@blackboard.com>
2018-07-03 15:26:17 +0800

    Travis integration

--- cpfile.sh
+++ cpfile.sh
@@ -1,6 +1,6 @@
-mkdir docs/static/imgs
+mkdir docs/static/imgs # Static files which needs to export to github pages 
 cp -r src/static/imgs/ docs/static/imgs/
 
-touch docs/CNAME
-echo "shanghai.freecodecamp.cn" >> docs/CNAME
-cp favicon.ico docs/favicon.ico
+touch docs/CNAME # Generating CNAME file for github pages to resolve DNS
+echo "shanghai.freecodecamp.cn" >> docs/CNAME # site hostname
+cp favicon.ico docs/favicon.ico # Override React simple app auto generated favicon
---
+++ deploy.sh
@@ -0,0 +1,13 @@
+if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+    echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
+    exit 0
+  else
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+      echo "Travis should not deploy from pull requests" # After test integrated 
+      exit 0  # here would trigger npm run test instead
+    else
+      git config user.name "${USER_NAME}"
+      git config user.email "${USER_EMAIL}"
+      git add .
+      git commit -m "Update github pages [skip ci]"
+      git push "https://${GITHUB_TOKEN}@${GH_REPO}" master
